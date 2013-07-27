bb9757391600286be821f8b2db8766b240de96e2
m_rastan <m.rachtan@gmail.com>
2013-07-27 21:19:39 +0200

    QS v1.1 RC

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var express = require('express')
   , routes = require('./routes')
   , user = require('./routes/user')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , mongo = require('mongodb');
 
 var app = express();
 
@@ -15,6 +16,9 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+app.set('mongodbUrl',  process.env.MONGOLAB_URI 
+					|| process.env.MONGOHQ_URL 
+					|| 'mongodb://localhost/qs_app');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -31,6 +35,7 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.post('/users/register', user.register);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,34 @@
  * GET users listing.
  */
 
+var mongo = require('mongodb'),
+	check = require('validator').check,
+    db;
+
 exports.list = function(req, res){
-  res.send("respond with a resource");
+  res.send("Access denied!");
+};
+
+exports.register = function(req, res) {
+	var email = req.body['email'];
+
+	try {
+		check(email).len(1, 128).isEmail();
+
+		mongo.Db.connect( req.app.get('mongodbUrl'), function (err, db) {
+  		db.collection('users', function(er, collection) {
+    		collection.insert( {'email'		: email,
+    							'timestamp' : Date.now() / 1000, 
+    							'client_ip'	: req.connection.remoteAddress,							
+    							'ua'		: req.headers['user-agent'],
+    							'lang'		: req.headers['accept-language']
+    						}, {safe: true}, function(er,rs) {
+    			res.send('User registered!');
+				});
+    		});
+	  	});	
+
+	} catch (e) {
+		res.send('Email - ' + e.message);
+	}
 };
